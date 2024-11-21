dc64eabe18f69af9535f0969684c751920abc584
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-11-22 06:56:18 +0700

    Update visualize_bouding_box.py

--- Ultilities/visualize_bouding_box.py
+++ Ultilities/visualize_bouding_box.py
@@ -47,6 +47,8 @@ image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.imshow(image_rgb)
 plt.axis("off")  # Hide axes
 plt.title("Labeled Image")
-plt.show()
+
 plt.savefig(f'outputs/v11l_IP038000453.jpg')  # Save the plot as an image file
+
+plt.show()
 plt.close()

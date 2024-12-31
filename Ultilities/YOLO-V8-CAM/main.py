77d7061e92b8bd10c7878df27af88d8b33ed927c
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-12-31 10:08:49 +0700

    Update main.py

--- Ultilities/YOLO-V8-CAM/main.py
+++ Ultilities/YOLO-V8-CAM/main.py
@@ -25,12 +25,12 @@ from yolo_cam.utils.image import show_cam_on_image, scale_cam_image
 # model = YOLO('models/ip102_yolov10l_625.pt')
 # model = YOLO('models/yolo11n.pt')
 # model = YOLO('models/ip102_yolov8x_69.7.pt')
-# model = YOLO('models/k4_69.4.pt')
+model = YOLO('models/k4_69.4.pt')
 # model = YOLO('models/k5_69.3.pt')
 # model = YOLO('models/r2000_81.1.pt')
 # model = YOLO('models/pest24_v9m.pt')
 # model = YOLO('models/biodetr.pt')   # activate biodetr first
-model = YOLO('models/rtdetr.pt')   # activate thinhdv first
+# model = YOLO('models/rtdetr.pt')   # activate thinhdv first
 
 
 model.cpu()
@@ -49,7 +49,7 @@ cam = EigenCAM(model, target_layers,task='od')
 grayscale_cam = cam(rgb_img)[0, :, :]
 cam_image = show_cam_on_image(img, grayscale_cam, use_rgb=True)
 plt.imshow(cam_image)
-plt.savefig(f'outputs/rtdetr.jpg')  # Save the plot as an image file
+plt.savefig(f'outputs/k4.jpg')  # Save the plot as an image file
 plt.show()
 plt.close()
 
